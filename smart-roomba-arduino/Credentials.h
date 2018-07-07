2c91ee5c678a5ceb2d72712a09ca1b0e0db9ad6c
Saikrishna Rao <krish98.sai@gmail.com>
2018-07-06 22:26:13 -0700

    Fixes bytes sent to Roomba for drive controls. Adds file to load credentials without commiting them.

---
+++ smart-roomba-arduino/Credentials.h
@@ -0,0 +1,13 @@
+#ifndef Credentials_h
+#define Credentials_h
+#include "Arduino.h"
+
+class Credentials
+{
+  public:
+    Morse();
+    char* ssid;
+    char* password;
+};
+
+#endif
--- smart-roomba-arduino/smart-roomba-arduino.ino
+++ smart-roomba-arduino/smart-roomba-arduino.ino
@@ -1,10 +1,12 @@
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
 #include <Roomba.h>
+#include <Credentials.h>
 
 // General WiFi credentials
-const char* ssid = "SSID";
-const char* password = "PASS";
+Credentials credentials();
+const char* ssid = credentials.ssid;
+const char* password = credentials.password;
 
 // UDP server config
 WiFiUDP UDPControlServer;
@@ -13,12 +15,12 @@ const int packetSize = 2;
 byte packet[packetSize];
 
 // Roomba selection
-Roomba roomba(&Serial, Roomba::Baud115200);
+//Roomba roomba(&Serial, Roomba::Baud115200);
 
 void connect_wifi(){
   WiFi.begin(ssid, password);
-  const IPAddress ip = IPAddress(192, 168, 1, 60);
-  const IPAddress ip_host = IPAddress(192, 168, 1, 1);
+  const IPAddress ip = IPAddress(10, 0, 0, 60);
+  const IPAddress ip_host = IPAddress(10, 0, 0, 1);
   const IPAddress ip_mask = IPAddress(255, 255, 255, 0);
   WiFi.config(ip, ip_host, ip_mask);
 
@@ -52,24 +54,103 @@ void callbackUDPServer(){
 
 void execCommand(String command){
   if(command.equals("00")){
-    roomba.start();
-    roomba.driveDirect(500, 500);
+    forwardDrive();
   } else if(command.equals("01")){
-    roomba.start();
-    roomba.driveDirect(0, 0);
+    backwardDrive();
+  } else if(command.equals("02")){
+    rotateRight();
+  } else if(command.equals("03")){
+    rotateLeft();
+  } else if(command.equals("CL")){
+    startCleaning();
+  } else if(command.equals("ST")){
+    stopCleaning();
   } else{
     // Do nothing
   }
 }
 
+void setupRoombaBaud(){
+  Serial.write(129);
+  delay(50);
+  Serial.write(11);
+  delay(50);
+}
+
+void startCleaning(){
+  Serial.write(136);
+  delay(50);
+  Serial.write(0);
+}
+
+void stopCleaning(){
+  Serial.write(136);
+  delay(50);
+  Serial.write(255);
+}
+
+void forwardDrive(){
+  Serial.write(137);
+  Serial.write((byte)0x00);
+  Serial.write(0xc8);
+  Serial.write(0x80);
+  Serial.write((byte)0x00);
+  delay(1000);
+  stopDrive();
+}
+
+void backwardDrive(){
+  Serial.write(137);
+  Serial.write(0xff);
+  Serial.write(0x38);
+  Serial.write(0x80);
+  Serial.write((byte)0x00);
+  delay(1000);
+  stopDrive();
+}
+
+void rotateRight(){
+  Serial.write(137);
+  Serial.write((byte)0x00);
+  Serial.write(0x0f);
+  Serial.write(0xff);
+  Serial.write(0xff);
+  delay(1000);
+  stopDrive();
+}
+
+void rotateLeft(){
+  Serial.write(137);
+  Serial.write((byte)0x00);
+  Serial.write(0x0f);
+  Serial.write((byte)0x00);
+  Serial.write(0x01);
+  delay(1000);
+  stopDrive();
+}
+
+void stopDrive(){
+  Serial.write(137);
+  Serial.write((byte)0x00);
+  Serial.write((byte)0x00);
+  Serial.write((byte)0x00);
+  Serial.write((byte)0x00);
+}
+
 void setup(){
   Serial.begin(115200);
-  delay(10);
+  //setupRoombaBaud();
   connect_wifi();
+  delay(50);
+  Serial.write(128); // listen io mode
+  delay(50);
+  startUDPServer();
+  delay(50);
+  Serial.write(132); // full mode
+  delay(50);
 }
 
 void loop(){
   callbackUDPServer();
   delay(1);
 }
-

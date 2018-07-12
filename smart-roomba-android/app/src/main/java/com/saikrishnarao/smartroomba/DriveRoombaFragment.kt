ebebe3d117a60bb3332b764d8e96c9e9a4fe8da8
Saikrishna Rao <krish98.sai@gmail.com>
2018-07-11 17:28:17 -0700

    Control buttons on manual drive fragment are *finally* positioned and rotated correctly. Adds credits to readme

--- smart-roomba-android/app/src/main/java/com/saikrishnarao/smartroomba/DriveRoombaFragment.kt
+++ smart-roomba-android/app/src/main/java/com/saikrishnarao/smartroomba/DriveRoombaFragment.kt
@@ -1,12 +1,16 @@
 package com.saikrishnarao.smartroomba
 
-import android.content.Context
-import android.net.Uri
+import android.graphics.Bitmap
+import android.graphics.Matrix
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_drive_roomba.*
+import android.graphics.BitmapFactory
+import android.graphics.drawable.BitmapDrawable
+
 
 class DriveRoombaFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
